7d061fb4001075852ad99af45c14c564cba9b1bb
JoãoVargas <joao.vargas@tiflux.com>
2024-10-29 08:03:26 -0300

    dashboard 2:48:55 / 11:51:23

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,7 +7,7 @@ import AuthModal from './AuthModal'
 
 export default function Navbar() {
   return (
-    <div className='flex py-5 items-center justify-between bg-green-500'>
+    <div className='flex py-5 items-center justify-between'>
       <Link href="/" className='flex items-center gap-2'>
         <Image src={Logo} alt='Logo' className='size-10'/>
         <h4 className='text-3xl font-semibold'>
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,20 @@
 import React, { ReactNode } from 'react'
-import { requireUser } from '../lib/hooks'
+import { signOut } from '../lib/auth'
+
 import Link from 'next/link'
 import Image from 'next/image'
 
 import Logo from '@/public/shresus.png'
 import DashboardLinks from '../components/DashboardLinks'
+import { ThemeToggle } from '../components/ThemeToggle'
 
-export default async function DefaultLayout({children}: {children: ReactNode}) {
+import { Menu } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { requireUser } from '../lib/hooks'
+
+export default async function DashboardLayout({children}: {children: ReactNode}) {
   const session = await requireUser()
 
   return (
@@ -28,6 +36,52 @@ export default async function DefaultLayout({children}: {children: ReactNode}) {
               </nav>
             </div>
           </div>
+        </div>
+        <div className='flex flex-col'>
+          <header className='flex h-14 items-center gap-4 border-b bg-muted px-4 lg:h-[60px] lg:px-6'>
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button className='md:hidden shrink-0' size='icon' variant='outline'>
+                  <Menu className='size-5'/>
+                </Button>
+              </SheetTrigger>
+              <SheetContent side='left' className='flex flex-col'>
+                <nav className='grid gap-2 mt-10'>
+                  <DashboardLinks />
+                </nav>
+              </SheetContent>
+            </Sheet>
+            <div className='ml-auto flex items-center gap-x-4'>
+              <ThemeToggle />
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant='secondary' size='icon' className='rounded-full'>
+                    <img src={session?.user?.image as string} alt='User' width={20} height={20} className='w-full h-full rounded-full bg-background'/>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align='end'>
+                  <DropdownMenuLabel>
+                    Menu
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem asChild className='cursor-pointer'>
+                    <Link href='/dashboard/settings'>Settingss</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild className='cursor-pointer'>
+                    <form className='w-full' action={async () => {
+                      'use server'
+                      await signOut()
+                    }}>
+                      <button className='w-full text-left'>Logout</button>
+                    </form>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </header>
+          <main className='flex felx-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
+            {children}
+          </main>
         </div>
       </div>
     </>
